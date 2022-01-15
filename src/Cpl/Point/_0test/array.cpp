a9247a846dac43358738489476d931beee7168f3
johnt <john.t.taylor@gmail.com>
2022-01-15 13:51:29 -0500

    integer array unit test passing

--- src/Cpl/Point/_0test/array.cpp
+++ src/Cpl/Point/_0test/array.cpp
@@ -63,7 +63,7 @@ protected:
 constexpr MyInt16Array::Id_T  appleId  = 0;
 constexpr MyInt16Array::Id_T  orangeId = 1;
 
-static const int16_t orangeInitVal_[NUM_ELEM] = { 1, 2, 3, 4, 5 };
+static const int16_t orangeInitVal_[NUM_ELEM] ={ 1, 2, 3, 4, 5 };
 
 static MyInt16Array apple_( appleId );
 static MyInt16Array orange_( orangeId, orangeInitVal_ );
@@ -82,14 +82,14 @@ TEST_CASE( "MyInt16Array" )
 
     SECTION( "read" )
     {
-        valid = orange_.read( value, NUM_ELEM  );
+        valid = orange_.read( value, NUM_ELEM );
         REQUIRE( valid == true );
         REQUIRE( memcmp( value, orangeInitVal_, sizeof( value ) ) == 0 );
 
-        valid = apple_.read( value, sizeof(value) );
+        valid = apple_.read( value, sizeof( value ) );
         REQUIRE( valid == false );
         int16_t writeVal[NUM_ELEM] ={ 1, };
-        apple_.write( writeVal, sizeof(writeVal)  );
+        apple_.write( writeVal, NUM_ELEM );
         valid = apple_.read( value, sizeof( value ) );
         REQUIRE( valid );
         REQUIRE( memcmp( value, writeVal, sizeof( value ) ) == 0 );
@@ -98,30 +98,37 @@ TEST_CASE( "MyInt16Array" )
         valid = apple_.read( value, sizeof( value ) );
         REQUIRE( valid == false );
 
-        REQUIRE( apple_.getSize() == sizeof(int16_t) * NUM_ELEM);
+        REQUIRE( apple_.getSize() == sizeof( int16_t ) * NUM_ELEM );
     }
 
     SECTION( "write" )
     {
-        int16_t writeVal[NUM_ELEM] = { 2,3,4,5,6 };
-        apple_.write( writeVal, sizeof(writeVal), Api::eLOCK );
+        int16_t writeVal[NUM_ELEM] ={ 2,3,4,5,6 };
+        apple_.write( writeVal, NUM_ELEM, Api::eLOCK );
         valid = apple_.read( value, sizeof( value ) );
         REQUIRE( valid );
-        REQUIRE( memcmp( value, writeVal, sizeof(value) ) == 0 );
+        REQUIRE( memcmp( value, writeVal, sizeof( value ) ) == 0 );
+
+        int16_t writePartial[3] ={ 20,30,40 };
+        int16_t expectedVal[NUM_ELEM] ={ 2, 20, 30, 40, 6 };
+        apple_.write( writePartial, 3, Api::eUNLOCK, 1 );
+        valid = apple_.read( value, sizeof( value ) );
+        REQUIRE( valid );
+        REQUIRE( memcmp( value, expectedVal, sizeof( value ) ) == 0 );
     }
 
-#if 0
     SECTION( "other" )
     {
-        apple_.write( "Hi" );
+        int16_t writeVal[NUM_ELEM] ={ 12,13,14,15,16 };
+        apple_.write( writeVal, NUM_ELEM );
         bool isValid;
         bool locked;
-        valid = apple_.get( valStr, isValid, locked );
-        REQUIRE( valStr == "Hi" );
+        valid = apple_.get( value, sizeof( value ), isValid, locked );
+        REQUIRE( memcmp( value, writeVal, sizeof( value ) ) == 0 );
         REQUIRE( valid == true );
         REQUIRE( locked == false );
         apple_.setInvalid( Api::eLOCK );
-        valid = apple_.get( value, NUM_ELEM , isValid, locked );
+        valid = apple_.get( value, sizeof( value ), isValid, locked );
         REQUIRE( valid == false );
         REQUIRE( locked == true );
         apple_.setLockState( Api::eUNLOCK );
@@ -131,7 +138,8 @@ TEST_CASE( "MyInt16Array" )
     {
         char buffer[256];
         bool truncated;
-        apple_.write( "Bobby" );
+        int16_t writeVal[NUM_ELEM] ={ 112,113,114,115,116 };
+        apple_.write( writeVal, NUM_ELEM );
         bool result = db.toJSON( appleId, buffer, sizeof( buffer ), truncated );
         CPL_SYSTEM_TRACE_MSG( SECT_, ("toJSON: [%s]", buffer) );
         REQUIRE( result );
@@ -139,21 +147,32 @@ TEST_CASE( "MyInt16Array" )
         StaticJsonDocument<1024> doc;
         DeserializationError err = deserializeJson( doc, buffer );
         REQUIRE( err == DeserializationError::Ok );
-        REQUIRE( strcmp( doc["val"], "Bobby" ) == 0 );
+        REQUIRE( strcmp( doc["val"]["elems"][0], "0x70" ) == 0 );
+        REQUIRE( strcmp( doc["val"]["elems"][1], "0x71" ) == 0 );
+        REQUIRE( strcmp( doc["val"]["elems"][2], "0x72" ) == 0 );
+        REQUIRE( strcmp( doc["val"]["elems"][3], "0x73" ) == 0 );
+        REQUIRE( strcmp( doc["val"]["elems"][4], "0x74" ) == 0 );
 
-        result = db.fromJSON( "{\"id\":0,\"val\":\" hi there \"}" );
+        result = db.fromJSON( "{id:0,val:{startIdx:0,elems:[\"50\",\"40\",\"30\",\"20\",\"10\"]}}" );
         REQUIRE( result );
-        valid  = apple_.read( valStr );
+        valid  = apple_.read( value, NUM_ELEM );
+        int16_t expectedValue[NUM_ELEM] ={ 0x50, 0x40, 0x30, 0x20, 0x10 };
         REQUIRE( valid );
-        REQUIRE( valStr == " hi there "  );
+        REQUIRE( memcmp( value, expectedValue, sizeof( value ) ) == 0 );
 
         result = db.fromJSON( "{\"id\":0,\"val\":123}" );
         REQUIRE( result == false );
-        valid  = apple_.read( valStr );
+        valid  = apple_.read( value, NUM_ELEM );
         REQUIRE( valid );
-        REQUIRE( valStr == " hi there " );
+        REQUIRE( memcmp( value, expectedValue, sizeof( value ) ) == 0 );
+
+        result = db.fromJSON( "{id:0,val:{startIdx:3,elems:[\"55\",\"44\"]}}" );
+        REQUIRE( result );
+        valid  = apple_.read( value, NUM_ELEM );
+        int16_t expectedValue2[NUM_ELEM] ={ 0x50, 0x40, 0x30, 0x55, 0x44};
+        REQUIRE( valid );
+        REQUIRE( memcmp( value, expectedValue2, sizeof( value ) ) == 0 );
     }
-#endif
 
     REQUIRE( Cpl::System::Shutdown_TS::getAndClearCounter() == 0u );
 }
