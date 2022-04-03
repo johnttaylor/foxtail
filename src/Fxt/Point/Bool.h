556900b3f3d72973f9ba1ef825c4df75d2ccd10a
johnt <john.t.taylor@gmail.com>
2022-04-03 14:07:35 -0400

    fix float test and add bool point

---
+++ src/Fxt/Point/Bool.h
@@ -0,0 +1,120 @@
+#ifndef Fxt_Point_Bool_h_
+#define Fxt_Point_Bool_h_
+/*-----------------------------------------------------------------------------
+* This file is part of the Colony.Core Project.  The Colony.Core Project is an
+* open source project with a BSD type of licensing agreement.  See the license
+* agreement (license.txt) in the top/ directory or on the Internet atH
+* http://integerfox.com/colony.core/license.txt
+*
+* Copyright (c) 2014-2022  John T. Taylor
+*
+* Redistributions of the source code must retain the above copyright notice.
+*----------------------------------------------------------------------------*/
+/** @file */
+
+
+#include "Fxt/Point/Basic_.h"
+#include "Cpl/Memory/Allocator.h"
+
+///
+namespace Fxt {
+///
+namespace Point {
+
+
+/** This class provides a concrete implementation for a Point who's data is a
+    bool.
+
+    The toJSON()/fromJSON format is:
+        \code
+
+        { name:"<mpname>", type:"<mptypestring>", valid:true|false, locked:true|false, val:true|false }
+
+        \endcode
+
+ */
+class Bool : public Basic_<bool>
+{
+public:
+    /// Simplify access the stateful data
+    typedef typename Basic_<bool>::Stateful_T StateBlock_T;
+
+public:
+    /** Constructor. Invalid Point.
+     */
+    Bool( uint32_t pointId, const char* pointName, Cpl::Memory::ContiguousAllocator& allocatorForPointStatefulData ) : Basic_<bool>(pointId, pointName, allocatorForPointStatefulData ) {}
+
+    /// Constructor. Valid Point.  Requires an initial value
+    Bool( uint32_t pointId, const char* pointName, Cpl::Memory::ContiguousAllocator& allocatorForPointStatefulData, bool initialValue ) : Basic_<bool>( pointId, pointName, allocatorForPointStatefulData, initialValue ) {}
+
+public:
+    /// Type safe write. See Fxt::Point::Api
+    virtual void write( bool newValue, Fxt::Point::Api::LockRequest_T lockRequest = Fxt::Point::Api::eNO_REQUEST ) noexcept
+    {
+        Fxt::Point::PointCommon_::write( &newValue, sizeof( bool ), lockRequest );
+    }
+
+    /// Short-cut for writing true
+    inline void set( Fxt::Point::Api::LockRequest_T lockRequest = Fxt::Point::Api::eNO_REQUEST ) noexcept { write( true, lockRequest ); }
+
+    /// Short-cut for writing false
+    inline void clear( Fxt::Point::Api::LockRequest_T lockRequest = Fxt::Point::Api::eNO_REQUEST ) noexcept { write( false, lockRequest ); }
+
+    /// Updates the MP's data from 'src'. Note: The lock state of 'src' is NOT-USED/IGNORED
+    virtual void write( Bool& src, Fxt::Point::Api::LockRequest_T lockRequest = Fxt::Point::Api::eNO_REQUEST ) noexcept 
+    {
+        if ( src.isNotValid() )
+        {
+            setInvalid( lockRequest );
+        }
+        else
+        {
+            // Note: src.m_data is NOT null because the MP is valid
+            write( ((StateBlock_T*)(src.m_state))->data, lockRequest );
+        }
+    }
+
+public:
+    ///  See Fxt::Point::Api
+    const char* getType() const noexcept { return "Fxt::Point::Bool"; }
+
+public:
+    /// Creates a concrete instance in the invalid state
+    static Api* create( Cpl::Memory::Allocator&             allocatorForPoints,
+                        uint32_t                            pointId,
+                        const char*                         pointName,
+                        Cpl::Memory::ContiguousAllocator&   allocatorForPointStatefulData ) 
+    { 
+        return PointCommon_::create<Bool>( allocatorForPoints, pointId, pointName, allocatorForPointStatefulData );
+    }
+
+public:
+    bool toJSON_( JsonDocument& doc, bool verbose ) noexcept
+    {
+        // Construct the 'val' key/value pair (as a HEX string)
+        doc["val"] = (((StateBlock_T*) PointCommon_::m_state)->data);
+        return true;
+    }
+
+    bool fromJSON_( JsonVariant& src, Fxt::Point::Api::LockRequest_T lockRequest, Cpl::Text::String* errorMsg ) noexcept
+    {
+        // Attempt to parse the value key/value pair
+        if ( src.is<bool>() == false )
+        {
+            if ( errorMsg )
+            {
+                *errorMsg = "Invalid syntax for the 'val' key/value pair";
+            }
+            return false;
+        }
+
+        write( src.as<bool>(), lockRequest );
+        return true;
+    }
+};
+
+
+
+};      // end namespaces
+};
+#endif  // end header latch
--- src/Fxt/Point/_0test/bool.cpp
+++ src/Fxt/Point/_0test/bool.cpp
@@ -12,12 +12,11 @@
 #include "Catch/catch.hpp"
 #include "Cpl/System/_testsupport/Shutdown_TS.h"
 #include "Fxt/Point/Database.h"
-#include "Fxt/Point/Float.h"
+#include "Fxt/Point/Bool.h"
 #include "Cpl/System/Trace.h"
 #include <string.h>
 #include "Cpl/Memory/HPool.h"
 #include "Cpl/Memory/LeanHeap.h"
-#include "Cpl/Math/real.h"
 #include <new>
 
 #define SECT_   "_0test"
@@ -28,7 +27,7 @@ using namespace Fxt::Point;
 #define MAX_POINTS  2
 
 
-#define ORANGE_INIT_VAL 7.5
+#define ORANGE_INIT_VAL true
 
 #define APPLE_ID        0
 #define APPLE_LABEL     "APPLE"
@@ -36,24 +35,24 @@ using namespace Fxt::Point;
 #define ORANGE_ID       1
 #define ORANGE_LABEL    "ORANGE"
 
-static size_t stateHeapMemory_[(sizeof( Float::StateBlock_T ) * MAX_POINTS + sizeof( size_t ) - 1) / sizeof( size_t )];
+static size_t stateHeapMemory_[(sizeof( Bool::StateBlock_T ) * MAX_POINTS + sizeof( size_t ) - 1) / sizeof( size_t )];
 
 ////////////////////////////////////////////////////////////////////////////////
-TEST_CASE( "Float" )
+TEST_CASE( "Bool" )
 {
     Cpl::System::Shutdown_TS::clearAndUseCounter();
     Database<MAX_POINTS>     db;
     Cpl::Memory::LeanHeap    stateHeap( stateHeapMemory_, sizeof( stateHeapMemory_ ) );
     bool                     valid;
-    float                   value;
+    bool                   value;
 
-    Float* apple = new(std::nothrow) Float( APPLE_ID, APPLE_LABEL, stateHeap );
+    Bool* apple = new(std::nothrow) Bool( APPLE_ID, APPLE_LABEL, stateHeap );
     REQUIRE( apple );
-    Float* orange = new(std::nothrow) Float( ORANGE_ID, ORANGE_LABEL, stateHeap, ORANGE_INIT_VAL );
+    Bool* orange = new(std::nothrow) Bool( ORANGE_ID, ORANGE_LABEL, stateHeap, ORANGE_INIT_VAL );
     REQUIRE( orange );
 
 
-    SECTION( "read" )
+    SECTION( "read/write/invalid" )
     {
         valid = orange->read( value );
         REQUIRE( valid == true );
@@ -61,39 +60,26 @@ TEST_CASE( "Float" )
 
         valid = apple->read( value );
         REQUIRE( valid == false );
-        apple->write( 1.1F );
+        apple->write( true );
         valid = apple->read( value );
         REQUIRE( valid );
-        REQUIRE( Cpl::Math::areFloatsEqual( value, 1.1F ) );
+        REQUIRE( value == true );
 
         apple->setInvalid();
         valid = apple->read( value );
         REQUIRE( valid == false );
 
-        REQUIRE( apple->getStatefulMemorySize() >= (sizeof( float ) + sizeof( bool ) * 2) );
-    }
+        apple->set();
+        valid = apple->read( value );
+        REQUIRE( valid  );
+        REQUIRE( value == true );
 
-    SECTION( "write" )
-    {
-        apple->write( 2.0F );
-        REQUIRE( apple->read( value ) );
-        REQUIRE( Cpl::Math::areFloatsEqual( value, 2.0F ) );
+        apple->clear();
+        valid = apple->read( value );
+        REQUIRE( valid );
+        REQUIRE( value == false );
 
-        apple->increment();
-        REQUIRE( apple->read( value ) );
-        REQUIRE( Cpl::Math::areFloatsEqual( value, 3.0F ) );
-
-        apple->increment( 3.0F );
-        REQUIRE( apple->read( value ) );
-        REQUIRE( Cpl::Math::areFloatsEqual( value, 3.0F + 3.0F ) );
-
-        apple->decrement();
-        REQUIRE( apple->read( value ) );
-        REQUIRE( Cpl::Math::areFloatsEqual( value, 3.0F + 3.0F - 1.0F ) );
-
-        apple->decrement( 3.0F );
-        REQUIRE( apple->read( value ) );
-        REQUIRE( Cpl::Math::areFloatsEqual( value, 2.0F ) );
+        REQUIRE( apple->getStatefulMemorySize() >= (sizeof( bool ) + sizeof(bool)*2) );
     }
 
     SECTION( "write2" )
@@ -117,8 +103,7 @@ TEST_CASE( "Float" )
     {
         char buffer[256];
         bool truncated;
-        Cpl::Text::FString<100> errMsg;
-        apple->write( 127.3F );
+        apple->write( true );
         REQUIRE( db.add( *apple ) );
         REQUIRE( db.add( *orange ) );
 
@@ -129,7 +114,7 @@ TEST_CASE( "Float" )
         StaticJsonDocument<1024> doc;
         DeserializationError err = deserializeJson( doc, buffer );
         REQUIRE( err == DeserializationError::Ok );
-        REQUIRE( Cpl::Math::areFloatsEqual( doc["val"], 127.3F ) );
+        REQUIRE( doc["val"] == true );
 
         orange->setInvalid();
         result = db.toJSON( ORANGE_ID, buffer, sizeof( buffer ), truncated, false );
@@ -137,27 +122,30 @@ TEST_CASE( "Float" )
         REQUIRE( result );
         REQUIRE( truncated == false );
 
-        result = db.fromJSON( "{\"id\":0,\"val\":3.14}" );
+        result = db.fromJSON( "{\"id\":0,\"val\":true}" );
         REQUIRE( apple->read( value ) );
-        REQUIRE( Cpl::Math::areFloatsEqual( value, 3.14F ) );
+        REQUIRE( value == true );
 
+        result = db.fromJSON( "{\"id\":0,\"val\":false}" );
+        REQUIRE( apple->read( value ) );
+        REQUIRE( value == false );
 
-        result = db.fromJSON( "{\"id\":0,\"val\":21,locked:true}" );
+        result = db.fromJSON( "{\"id\":0,\"val\":true,locked:true}" );
         REQUIRE( result );
         REQUIRE( apple->read( value ) );
-        REQUIRE( Cpl::Math::areFloatsEqual( value, 21.0F ) );
+        REQUIRE( value == true );
         REQUIRE( apple->isLocked() );
 
-        result = db.fromJSON( "{\"id\":0,\"val\":22,locked:false}" );
+        result = db.fromJSON( "{\"id\":0,\"val\":false,locked:false}" );
         REQUIRE( result );
         REQUIRE( apple->read( value ) );
-        REQUIRE( Cpl::Math::areFloatsEqual( value, 22.0F ) );
+        REQUIRE( value == false );
         REQUIRE( !apple->isLocked() );
 
         result = db.fromJSON( "{\"id\":0,locked:true}" );
         REQUIRE( result );
         REQUIRE( apple->read( value ) );
-        REQUIRE( Cpl::Math::areFloatsEqual( value, 22.0F ) );
+        REQUIRE( value == false );
         REQUIRE( apple->isLocked() );
 
         result = db.fromJSON( "{\"id\":0,locked:false,valid:false}" );
@@ -166,47 +154,47 @@ TEST_CASE( "Float" )
         REQUIRE( !apple->isLocked() );
 
         // ERROR
-        result = db.fromJSON( "{\"id\":100,\"val\":\"0x20\",locked:true}" );
+        result = db.fromJSON( "{\"id\":100,\"val\":true,locked:true}" );
         REQUIRE( result == false );
 
         // ERROR
-        errMsg = "NOERROR";
-        result = db.fromJSON( "{\"id\":100,\"val\":\"0x20\",locked:true}", &errMsg );
-        REQUIRE( result == false );
-        REQUIRE( errMsg != "NOERROR" );
-
-        // ERROR
-        errMsg = "NOERROR";
-        result = db.fromJSON( "{\"id\":0,\"val\":true,locked:true}", &errMsg );
+        Cpl::Text::FString<100> errMsg = "NOERROR";
+        result = db.fromJSON( "{\"id\":100,\"val\":false,locked:true}", &errMsg );
         REQUIRE( result == false );
         REQUIRE( errMsg != "NOERROR" );
 
         // ERROR
-        result = db.fromJSON( "{\"id\":0,\"val\":false,locked:true}" );
-        REQUIRE( result == false );
-
-        // ERROR
-        result = db.toJSON( ORANGE_ID + 2, buffer, sizeof( buffer ), truncated, false );
-        REQUIRE( result == false );
-
-        // ERROR
         errMsg = "NOERROR";
-        result = db.fromJSON( "{\"id\"=100,\"val\":3.14,locked:true}", &errMsg );
+        result = db.fromJSON( "{\"id\":0,\"val\":20.1,locked:true}", &errMsg );
         REQUIRE( result == false );
         REQUIRE( errMsg != "NOERROR" );
 
         // ERROR
-        result = db.fromJSON( "{\"id\"=100,\"val\":3.14,locked:true}" );
+        result = db.fromJSON( "{\"id\":0,\"val\":20.1,locked:true}" );
+        REQUIRE( result == false );
+
+        // ERROR
+        result = db.toJSON( ORANGE_ID+2, buffer, sizeof( buffer ), truncated, false );
         REQUIRE( result == false );
 
         // ERROR
         errMsg = "NOERROR";
-        result = db.fromJSON( "{\"val\":3.14,locked:true}", &errMsg );
+        result = db.fromJSON( "{\"id\"=100,\"val\":true,locked:true}", &errMsg );
         REQUIRE( result == false );
         REQUIRE( errMsg != "NOERROR" );
 
         // ERROR
-        result = db.fromJSON( "{\"val\":3.14,locked:true}" );
+        result = db.fromJSON( "{\"id\"=100,\"val\":\true,locked:true}" );
+        REQUIRE( result == false );
+
+        // ERROR
+        errMsg = "NOERROR";
+        result = db.fromJSON( "{\"val\":false,locked:true}", &errMsg );
+        REQUIRE( result == false );
+        REQUIRE( errMsg != "NOERROR" );
+
+        // ERROR
+        result = db.fromJSON( "{\"val\":false,locked:true}" );
         REQUIRE( result == false );
 
         // ERROR
@@ -216,7 +204,7 @@ TEST_CASE( "Float" )
         REQUIRE( errMsg != "NOERROR" );
 
         // ERROR
-        result = db.fromJSON( "{\"id\":0}" );
+        result = db.fromJSON( "{\"id\":0}");
         REQUIRE( result == false );
     }
 
@@ -229,13 +217,13 @@ TEST_CASE( "Float" )
 
         REQUIRE( db.lookupById( ORANGE_ID ) == orange );
         REQUIRE( db.lookupById( APPLE_ID ) == apple );
-        REQUIRE( db.lookupById( ORANGE_ID + 2 ) == nullptr );
+        REQUIRE( db.lookupById( ORANGE_ID+2 ) == nullptr );
 
         REQUIRE( db.first() == apple );
         REQUIRE( db.next( *apple ) == orange );
         REQUIRE( db.next( *orange ) == nullptr );
     }
-
+    
     delete apple;
     delete orange;
     REQUIRE( Cpl::System::Shutdown_TS::getAndClearCounter() == 0u );
